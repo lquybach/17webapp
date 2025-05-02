deadca0c5a282ffee5ea8e0cdf133f3169048ccc
Chihiro Tone <40935648+toneDrei79@users.noreply.github.com>
2025-05-02 10:49:27 +0900

    add func route of get requests by user

--- function_app.py
+++ function_app.py
@@ -1,5 +1,5 @@
 import azure.functions as func
-from functions.requests import post_request, get_requests
+from functions.requests import post_request, get_requests, get_requests_by_user
 from functions.samples import get_samples
 from function.shipping_address_service import get_shipping_addresses
 
@@ -20,16 +20,18 @@ def _get_requests(req: func.HttpRequest) -> func.HttpResponse:
     return get_requests(req)
 
 
-# @app.route(route='get_requests', methods=['GET'])
-# @app.function_name(name='GetRequests')
-# def _get_requests(req: func.HttpRequest) -> func.HttpResponse:
-#     return get_requests(req)
+@app.route(route='get_requests_by_user', methods=['GET'])
+@app.function_name(name='GetRequestsByUser')
+def _get_requests_by_user(req: func.HttpRequest) -> func.HttpResponse:
+    return get_requests_by_user(req)
+
 
 # Samples endpoints
 @app.route(route='samples', methods=['GET'])
 @app.function_name(name='GetSamples')
 def _get_samples(req: func.HttpRequest) -> func.HttpResponse:
     return get_samples(req)
+
 
 # Shipping Addresses endpoint
 @app.route(route='shipping_addresses', methods=['GET'])
